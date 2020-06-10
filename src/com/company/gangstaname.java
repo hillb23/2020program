acf6d5032fe94c8f5e6678237fa98011c4395e29
Bridget <hillb23@greenhill.org>
2020-06-10 09:34:31 -0500

    homework for 6/11

---
+++ src/com/company/gangstaname.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+public class gangstaname {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.print("Type your name, playa: ");
+        String name = console.nextLine();
+        //split name into first and last initials
+        String first = name.substring(0, name.indexOf (" "));
+        String last = name.substring(name.indexOf(" ")+1);
+        last = last.toUpperCase();
+        String fInitial = first.substring(0,1);
+        //print out gangsta name output
+        System.out.println("Your gangsta name is \"" + fInitial + ". Diddy" + last + " " + first + "-izzle\"");
+
+
+
+    }
+}
